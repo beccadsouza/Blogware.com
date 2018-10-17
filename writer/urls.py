1b410cf394b46ecbc7e5ebd6d8fc188024c2ac2c
Rebecca D'souza <beccadsouza16@gmail.com>
2018-10-17 22:41:49 +0530

    ..

---
+++ writer/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path,include
+from users import urls
+from  . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path(r'caterreq/', views.caterreq),
+    path(r'draft/',views.draft),
+    #path(r'edit/',views.edit)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
