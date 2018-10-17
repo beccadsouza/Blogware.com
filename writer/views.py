f1c76f81f59026906cdfbe42f597cff35145d614
Rebecca D'souza <beccadsouza16@gmail.com>
2018-10-17 15:40:32 +0530

    some more changes

--- writer/views.py
+++ writer/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from
 # Create your views here.
+def draft(request):
+    if request.method == "POST":
+        if "forward" in request.POST:
+            print('Do something')
